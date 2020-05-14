97108483558117341acb93be73d6b2908d9b83ac
vainjoker <vainjoker@163.com>
2020-05-14 09:24:09 +0800

    day++

---
+++ Signal and System/.auctex-auto/class2.el
@@ -0,0 +1,11 @@
+(TeX-add-style-hook
+ "class2"
+ (lambda ()
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art10"
+    "ctex"
+    "fancybox"))
+ :latex)
+
---
+++ Signal and System/auto/class5.el
@@ -0,0 +1,10 @@
+(TeX-add-style-hook
+ "class5"
+ (lambda ()
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art10"
+    "ctex"))
+ :latex)
+
